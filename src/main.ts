d5921b7972417d4afe9f2abd8b5db0ca53ea4e86
braddoubleu <braddoubleu@gmail.com>
2024-04-10 22:34:37 -0700

    updated commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 // This is the preferred way to include a css file.
 import "./style.css";
 
-import { getById } from "phil-lib/client-misc";
+//import { getById } from "phil-lib/client-misc";
 
-// getById() is usually the first function I import.
-// This is the best way to access HTML from TypeScript most of the time.
-const typescriptStatusSpan = getById("typescriptStatus", HTMLSpanElement);
-typescriptStatusSpan.innerText = "working";
+document.body.append("Premier Idea Generation Technology")
